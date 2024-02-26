ada14ba77ad56acea7fdd46bd1b76188071f4f6a
Johnny Benitez <johnnybeniitez@gmail.com>
2024-02-26 17:46:28 -0300

    método crearUsuario() en Controladora

--- ConsultorioOdontologico/src/main/java/logica/Controladora.java
+++ ConsultorioOdontologico/src/main/java/logica/Controladora.java
@@ -6,4 +6,12 @@ public class Controladora {
     
     ControladoraPersistencia persis = new ControladoraPersistencia();
     
+    public void crearUsuario(int id, String nombre_user, String contrasenia, String rol){
+        
+        Usuario user = new Usuario(id, nombre_user, contrasenia, rol);
+        persis.crearUsuario(user);
+        
+    }
+    
+    
 }
--- ConsultorioOdontologico/src/main/java/persistencia/ControladoraPersistencia.java
+++ ConsultorioOdontologico/src/main/java/persistencia/ControladoraPersistencia.java
@@ -1,13 +1,19 @@
 package persistencia;
 
+import logica.Usuario;
+
 public class ControladoraPersistencia {
     
     HorarioJpaController horaJpa = new HorarioJpaController();
     OdontologoJpaController odoJpa = new OdontologoJpaController();
     PacienteJpaController paciJpa = new PacienteJpaController();
-    ResponsableJpaController respoJpa = new ResponsableJpaController();
+    ResponsableJpaController respJpa = new ResponsableJpaController();
     SecretarioJpaController secreJpa = new SecretarioJpaController();
     TurnoJpaController turnoJpa = new TurnoJpaController();
     UsuarioJpaController userJpa = new UsuarioJpaController();
+
+    public void crearUsuario(Usuario user) {
+        userJpa.create(user);
+    }
     
 }
