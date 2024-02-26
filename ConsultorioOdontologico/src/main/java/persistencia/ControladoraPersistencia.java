ffe255af6e62d45a3bb1336f2e34520aad5b6d11
Johnny Benitez <johnnybeniitez@gmail.com>
2024-02-26 17:34:47 -0300

    Instancias de los JpaController en la ControladoraPersistencia

--- ConsultorioOdontologico/src/main/java/persistencia/ControladoraPersistencia.java
+++ ConsultorioOdontologico/src/main/java/persistencia/ControladoraPersistencia.java
@@ -2,4 +2,12 @@ package persistencia;
 
 public class ControladoraPersistencia {
     
+    HorarioJpaController horaJpa = new HorarioJpaController();
+    OdontologoJpaController odoJpa = new OdontologoJpaController();
+    PacienteJpaController paciJpa = new PacienteJpaController();
+    ResponsableJpaController respoJpa = new ResponsableJpaController();
+    SecretarioJpaController secreJpa = new SecretarioJpaController();
+    TurnoJpaController turnoJpa = new TurnoJpaController();
+    UsuarioJpaController userJpa = new UsuarioJpaController();
+    
 }
