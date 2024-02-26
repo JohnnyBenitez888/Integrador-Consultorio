b3301c331039a8a230931f531e9ccd9285ef97d0
Johnny Benitez <johnnybeniitez@gmail.com>
2024-02-25 22:53:58 -0300

    Enum Especialidad, Clase Secretario

--- ConsultorioOdontologico/src/main/java/logica/Odontologo.java
+++ ConsultorioOdontologico/src/main/java/logica/Odontologo.java
@@ -1,13 +1,70 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package logica;
 
-/**
- *
- * @author Johnny
- */
-public class Odontologo {
+import java.util.Date;
+import java.util.List;
+
+
+public class Odontologo extends Persona{
+    
+    private int id_odontologo;
+    private Especialidad especialidad;
+    private List<Turno> turnos;
+    private Usuario unUsuario;
+    private Horario unHorario;
+
+    public Odontologo() {
+    }
+
+    public Odontologo(int id_odontologo, Especialidad especialidad, List<Turno> turnos, Usuario unUsuario, Horario unHorario, String dni, String nombre, String apellido, String telefono, String direccion, Date fecha_nac) {
+        super(dni, nombre, apellido, telefono, direccion, fecha_nac);
+        this.id_odontologo = id_odontologo;
+        this.especialidad = especialidad;
+        this.turnos = turnos;
+        this.unUsuario = unUsuario;
+        this.unHorario = unHorario;
+    }
+
+    public int getId_odontologo() {
+        return id_odontologo;
+    }
+
+    public void setId_odontologo(int id_odontologo) {
+        this.id_odontologo = id_odontologo;
+    }
+
+    public Especialidad getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(Especialidad especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public List<Turno> getTurnos() {
+        return turnos;
+    }
+
+    public void setTurnos(List<Turno> turnos) {
+        this.turnos = turnos;
+    }
+
+    public Usuario getUnUsuario() {
+        return unUsuario;
+    }
+
+    public void setUnUsuario(Usuario unUsuario) {
+        this.unUsuario = unUsuario;
+    }
+
+    public Horario getUnHorario() {
+        return unHorario;
+    }
+
+    public void setUnHorario(Horario unHorario) {
+        this.unHorario = unHorario;
+    }
+    
+    
+    
     
 }
